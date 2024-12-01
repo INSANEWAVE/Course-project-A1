c80ca2dcdb39b0c90b3568d5f222a953fe4d38ea
INSANEWAVE <alxbelter@outlook.com>
2024-12-02 00:08:17 +0300

    Комментарии, оптимизация кода

--- Main.ino
+++ Main.ino
@@ -1,12 +1,12 @@
 unsigned long next_time;
-unsigned long second_next_time; // время очередного переключения первого светодиода
-int timeout = 10;
-int timeout2 = 1000; // половина периода мигания
+unsigned long second_next_time; 
+int timeout = 5; // Частота обновления датчика оборотов
+int timeout2 = 1000; //Частота вычисления оборотов в секунду
 int sensorPin = A0;   // Пин, к которому подключен фоторезистор
 int sensorValue = 0;   // Переменная для хранения значения с фоторезистора
-int counter = 0;
+int counter = 0; //Счётчик оборотов
 void setup() {
-  Serial.begin(9600);   // Инициализация Serial для вывода данных на монитор
+  Serial.begin(9600);   // Инициализация Serial для вывода данных.
   next_time = millis() + timeout;
   second_next_time = millis() + timeout2; // вычисляем время следующего переключения
 }
@@ -25,8 +25,10 @@ void loop() {
          }
    if( now_time >= second_next_time ){               
         second_next_time = now_time + timeout2;
-          float RPM = counter/0.5;
-          Serial.println(RPM);
+          float RPM = counter/(timeout2/1000);    
+          Serial.println(RPM);    // 02.12.2024 Тут нужно сделать вывод на сервопривод. В идеале использовать функцию map
+                                  // Стрелочный индикатор на 360 градусов. 
+                                  //Также нужно уменьшить зеркальный элемент на роторе и решить проблему с зашкаливанием датчика - сейчас крайнее значение 200 оборотов в секунду.
           counter = 0;
          } 
         
